5fd48e6a58c8720d3c03128e2b3813bf5777ac35
Elijah Trillionz <elijahtrillionz@gmail.com>
2021-12-30 19:29:59 +0100

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { TolgeeProvider } from '@tolgee/react';
+import { UI } from '@tolgee/ui';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <TolgeeProvider
+      filesUrlPrefix='i18n/'
+      apiUrl={process.env.REACT_APP_TOLGEE_API_URL}
+      apiKey={process.env.REACT_APP_TOLGEE_API_KEY}
+      ui={process.env.REACT_APP_TOLGEE_API_KEY ? UI : undefined}
+      loadingFallback='Loading...'
+    >
+      <Component {...pageProps} />
+    </TolgeeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
